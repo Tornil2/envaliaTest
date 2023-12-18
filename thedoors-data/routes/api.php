f87a75cf7f5f68f0c842268470854e705b5aa2e7
maarblan <maarblan@gmail.com>
2023-12-18 02:03:07 +0100

    reload

--- thedoors-data/routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ItemsController;
-use App\Http\Controllers\CategoriesController;
-
-Route::apiResource('items', ItemsController::class);
-Route::apiResource('categories', CategoriesController::class);
